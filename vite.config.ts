017a5a8fe01d5755d6b259480c04ad3321c1a3a9
albertoomartinn <albertoomartin18@gmail.com>
2025-07-10 14:55:44 +0200

    feat: implement storybook and configure tsconfig.vitest.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,22 @@
-import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
+import { resolve } from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), vueDevTools(), tailwindcss()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@': resolve(__dirname, './src'),
+    },
+  },
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: ['./tests/setup.ts'],
+    alias: {
+      '@': resolve(__dirname, './src'),
     },
   },
 })
