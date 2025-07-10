b980146db2c43d19f7b44ad9464093edc9fd7c36
albertoomartinn <albertoomartin18@gmail.com>
2025-07-10 15:34:15 +0200

    feat: TheHeader with stories

---
+++ src/components/organisms/TheHeader.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+defineProps({
+  title: { type: String, required: true },
+})
+</script>
+<template>
+  <div
+    class="text-center text-3xl font-bold text-sky-steel p-4 bg-navy-night border-b border-gray-300"
+  >
+    {{ title }}
+  </div>
+</template>
